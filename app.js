3a87dc0bbddf7f98021628ac3b1b62e5c0d56fbf
chehak123 <chehakagrawal01@gmail.com>
2021-01-20 10:58:35 +0530

    minor changes

--- app.js
+++ app.js
@@ -11,33 +11,9 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// app.use(session({
-//   secret: "Our little secret.",
-//   resave: false,
-//   saveUninitialized: false
-// }));
-
-
-
 app.get("/", function(req, res){
   res.render("firstPage");
 });
-
-// app.get("/home", function(req, res){
-//   res.render("home");
-// });
-
-// app.get("/register", function(req, res){
-//   res.render("register");
-// });
-
-// app.get("/login", function(req, res){
-//   res.render("login");
-// });
-
-// app.get("/register", function(req, res){
-//   res.render("register");
-// });
 
 app.get("/secrets", function(req, res){
   User.find({"secret": {$ne: null}}, function(err, foundUsers){
