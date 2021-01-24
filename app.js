72f035453255dec4f09dda5aa55ad187c8798480
chehak123 <chehakagrawal01@gmail.com>
2021-01-24 13:43:01 +0530

    minor changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000
 const app = express();
 
 app.use(express.static("public"));
@@ -15,17 +15,17 @@ app.get("/", function(req, res){
   res.render("firstPage");
 });
 
-app.get("/secrets", function(req, res){
-  User.find({"secret": {$ne: null}}, function(err, foundUsers){
-    if (err){
-      console.log(err);
-    } else {
-      if (foundUsers) {
-        res.render("secrets", {usersWithSecrets: foundUsers});
-      }
-    }
-  });
-});
+// app.get("/secrets", function(req, res){
+//   User.find({"secret": {$ne: null}}, function(err, foundUsers){
+//     if (err){
+//       console.log(err);
+//     } else {
+//       if (foundUsers) {
+//         res.render("secrets", {usersWithSecrets: foundUsers});
+//       }
+//     }
+//   });
+// });
 
 app.listen(port, function() {
   console.log("Server started on port 3000.");
