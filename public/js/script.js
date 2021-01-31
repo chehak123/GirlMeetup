866725d4304852651700324a674f7485c59f48dc
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-01-31 22:22:33 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -12,4 +12,23 @@ wow = new WOW(
     var section = document.createElement('section');
     section.className = 'section--purple wow fadeInDown';
     this.parentNode.insertBefore(section, this);
-  };
+
+
+/* WOW animations */
+new WOW().init();
+
+
+/* FAQ Accordion Up and down arrows */
+  $(document).ready(function(){
+          // Add down arrow icon for collapse element which is open by default
+          $(".collapse.show").each(function(){
+            $(this).prev(".card-header").find(".fa").addClass("fa-angle-down").removeClass("fa-angle-right");
+          });
+          
+          // Toggle right and down arrow icon on show hide of collapse element
+          $(".collapse").on('show.bs.collapse', function(){
+            $(this).prev(".card-header").find(".fa").removeClass("fa-angle-right").addClass("fa-angle-down");
+          }).on('hide.bs.collapse', function(){
+            $(this).prev(".card-header").find(".fa").removeClass("fa-angle-down").addClass("fa-angle-right");
+          });
+      });
