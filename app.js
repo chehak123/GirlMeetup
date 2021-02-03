66c246596941d99e435d83fd6df745beb3966164
KarunKarthik <kkarun0542@gmail.com>
2021-02-03 11:47:50 +0530

    Added blog page
    Signed-off-by: KarunKarthik <kkarun0542@gmail.com>

--- app.js
+++ app.js
@@ -104,6 +104,10 @@ app.get("/faq", (req, res) => {
   res.render("faq",{currentUser: req.user});
 });
 
+app.get("/blog", (req, res) => {
+  res.render("blog",{currentUser: req.user});
+});
+
 
 app.listen(port, function() {
   console.log("Server started on port 3000.");
